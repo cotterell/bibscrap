9aaf8549c74175c4e1d15effd166877e0f830f1c
Matthew Pooser <mpooser@gmail.com>
2020-09-03 10:26:43 -0400

    Source code so far

---
+++ bibscrap/spiders/acmdl.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class AcmdlSpider(scrapy.Spider):
+    name = 'acmdl'
+    #allowed_domains = ['acm.org']
+    #start_urls = ['http://dl.acm.org/doi/10.1145/2666652.2666656']
+
+
+    def __init__(self, doi='', **kwargs):
+        self.start_urls = [f'https://dl.acm.org/doi/{doi}']  # py36
+        #self.start_urls = [f'http://dl.acm.org/doi/{doi}']
+        super().__init__(**kwargs)  # python3
+
+
+    def parse(self, response):
+        sel = scrapy.Selector(response)
+        title = sel.xpath('/html/body/div[1]/div/main/div[2]/article/div[1]/div[2]/div/div[2]/h1/text()').extract()
+        author = sel.xpath('/html/body/div[1]/div/main/div[2]/article/div[1]/div[2]/div/div[3]/div/ul/li[2]/a/span/div/span/span/text()').extract()
+        abstract = sel.xpath('/html/body/div[1]/div/main/div[2]/article/div[2]/div[2]/div[2]/div[1]/div/div[2]/p/text()').extract()
+
+        print(f'title: {title}')
+        print(f'author: {author}')
+        print(f'abstract: {abstract}')
---
+++ bibscrap/spiders/ieeexplore.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.selector import Selector
+
+class IeeexploreSpider(scrapy.Spider):
+    name = 'ieeexplore'
+    allowed_domains = ['ieee.org', 'doi.org']
+    start_urls = ['https://ieeexplore.ieee.org/document/5581052']
+
+    #def __init__(self, doi='', **kwargs):
+    #    self.start_urls = [f'https://dx.doi.org/{doi}']  # py36
+    #    super().__init__(**kwargs)  # python3
+
+
+    def parse(self, response):
+        #self.log(self.domain)
+        hxs = scrapy.Selector(response)
+        titles = hxs.xpath('/html/body/div[4]/div/div/div/div[5]/div/xpl-root/div/xpl-document-details/div/div[1]/section[2]/div/xpl-document-header/section/div[2]/div/div/div[1]/div/div/h1/span/text()')
+        print(titles.extract())
