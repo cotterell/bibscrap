44e6efc199ca2328f5069fc580d8085f418a73be
mynguyen0628 <70764413+mynguyen0628@users.noreply.github.com>
2020-10-09 14:36:47 -0400

    Update ieeexplore.py

--- bibscrap/spiders/ieeexplore.py
+++ bibscrap/spiders/ieeexplore.py
@@ -18,15 +18,20 @@ class IeeexploreSpider(scrapy.Spider):
         titles = hxs.xpath('/html/body/div[4]/div/div/div/div[5]/div/xpl-root/div/xpl-document-details/div/div[1]/section[2]/div/xpl-document-header/section/div[2]/div/div/div[1]/div/div/h1/span/text()')
         print(titles.extract())
 
- import json
+ import json, urllib.request
 
 #how to import json?
 #user put in doi as a parameter, then we import json of the website using query + api key + &doi=
 #then work the parse json data
 #there is a limit to how many call a day when using api key, might have to register for some more api keys
- with open('data.json') as json_file:
-        data = json.load(json_file)
-        
+
+ with urllib.request.urlopen("https://ieeexploreapi.ieee.org/api/v1/search/articles?parameter&apikey=3r88q7n22u429vtenyjjrhks&doi=10.1109/TTS.2020.2992669") as url:
+    data = json.loads(url.read().decode())
+       
+    for i in data['article']:
         print("Title: ", data['title'])
-        print("Authors: ", data['authors')
+            
+        for j in data['authors']:
+            print("Authors: ", data['authors'])
+            
         print("Abstract:", data['abstract'])
