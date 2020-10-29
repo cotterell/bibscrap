b240533163e7161e23a751ddcf8b97eeb0e118d8
mynguyen0628 <70764413+mynguyen0628@users.noreply.github.com>
2020-10-28 23:02:03 -0400

    Update ieeexplore.py

--- bibscrap/spiders/ieeexplore.py
+++ bibscrap/spiders/ieeexplore.py
@@ -21,50 +21,55 @@ class IeeexploreSpider(scrapy.Spider):
  
 
 
-
+import scrapy
 import json, urllib.request
 import ssl
 import pandas as pd
 
+class IeeexploreSpider(scrapy.Spider):
+    name = 'ieee'
+    
+    #doi_shneiderman = '10.1109/TTS.2020.2992669'
+    
+    def __init__(self, doi='', **kwargs):
+        global doiRef = doi
+        get_ieee_paper(doiRef)
 
-doi_shneiderman = '10.1109/TTS.2020.2992669'
+    def get_ieee_paper_dict(doi):
+        api_key = '3r88q7n22u429vtenyjjrhks'
+        url = f'https://ieeexploreapi.ieee.org/api/v1/search/articles?parameter&apikey={api_key}&doi={doi}'
+        context_ssl = ssl._create_unverified_context()
 
-def get_ieee_paper_dict(doi):
-    api_key = '3r88q7n22u429vtenyjjrhks'
-    url = f'https://ieeexploreapi.ieee.org/api/v1/search/articles?parameter&apikey={api_key}&doi={doi}'
-    context_ssl = ssl._create_unverified_context()
-    
-    try:
-        with urllib.request.urlopen(url, context = context_ssl) as url:
-            data = json.loads(url.read().decode())
-            return data
-    except Exception as e: 
-        print(e)
-    
-    return {}
+        try:
+            with urllib.request.urlopen(url, context = context_ssl) as url:
+                data = json.loads(url.read().decode())
+                return data
+        except Exception as e: 
+            print(e)
 
-def get_ieee_paper(doi):
-    data = get_ieee_paper_dict(doi)
-    
-    article_data = data['articles']
-    title = article_data[0]["title"]
-    
-    authors_data = article_data[0]['authors']['authors']
-    authors_full_name = ''
-    for i in authors_data:
-        authors_full_name += i['full_name'] +', '
-    
-    abstract = article_data[0]['abstract']
-    
-    final_data = [{
-        'DOI': doi,
-        'Title': title,
-        'Authors': authors_full_name,
-        'Abstract': abstract,
-    }]
-    return final_data
+        return {}
 
-def convert_to_dataframe(data):
-    df = pd.DataFrame(data, columns = ['DOI', 'Title', 'Authors', 'Abstract'])
-    return df
+    def get_ieee_paper(doi):
+        data = get_ieee_paper_dict(doi)
+
+        article_data = data['articles']
+        title = article_data[0]["title"]
+
+        authors_data = article_data[0]['authors']['authors']
+        authors_full_name = ''
+        for i in authors_data:
+            authors_full_name += i['full_name'] +', '
+
+        abstract = article_data[0]['abstract']
+
+        final_data = [{
+            'DOI': doi,
+            'Title': title,
+            'Authors': authors_full_name,
+            'Abstract': abstract,
+        }]
+
+        df = pd.DataFrame(final_data, columns = ['DOI', 'Title', 'Authors', 'Abstract'])
+        
+        df.to_csv(f'{doi}.csv')
 
