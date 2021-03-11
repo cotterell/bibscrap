51a8602873a994ea24c8e5893ebea3e96c36b9c9
mynguyen0628 <70764413+mynguyen0628@users.noreply.github.com>
2021-03-11 14:28:10 -0500

    Update ieeexplore.py

--- bibscrap/spiders/ieeexplore.py
+++ bibscrap/spiders/ieeexplore.py
@@ -9,7 +9,7 @@ import ssl
 import pandas as pd
 from pprint import pprint
 
-class IeeexploreSpider(scrapy.Spider):
+class Ieee(BibscrapSpider, scrapy.Spider):
     name = 'ieee'
     allowed_domains = ['ieee.org']
     _current_key = 0
@@ -25,7 +25,7 @@ class IeeexploreSpider(scrapy.Spider):
         api_key = api_keys[_current_key % len(api_keys)]
         _current_key += 1
         return api_key
-
+    
     def get_ieee_paper_dict(self, doi=None, article_title=None):
         api_key = self.get_api_key()
         if article_title == None: 
@@ -42,35 +42,19 @@ class IeeexploreSpider(scrapy.Spider):
             print(e)
 
         return {}
-
+    
     def concat(self, iterable):  
         out = ''
         for item in iterable:
             out += item
         return out
 
-
     def clean_reference(self, entry):
         reference = self.concat(c for c in entry if c not in ['\n', '\t'])  #adding up the strings and omitting \n and \t in each entry/article      
         period = reference.find('.')
         reference = reference[period+1:]                             # omit order number and first period
         return reference
-
-
-    def ieee_references(self, ar_number):
-        url = f'https://ieeexplore.ieee.org/xpl/dwnldReferences?arnumber={ar_number}'
-        html = urlopen(url).read()
-        soup = BeautifulSoup(html, features="html.parser")
-        # kill all script and style elements
-        for script in soup(["script", "style"]):
-            script.extract()    # rip it out
-        # get text
-        text = soup.get_text()   
-        #pprint(concat(c for c in text if c not in ['\n', '\t']))
-        entries= text.split('\t    \t\t\t\n')                        #splitting the articles and putting them into a list
-        entries= entries[1:]                                         # omit "View References"
-        return [self.clean_reference(entry) for entry in entries]         #cleaning each reference and put them into a list
-
+    
     def get_article_name_from_ref(self, entry):
         begin_quo = entry.find('"') 
         article_name = ''
@@ -80,8 +64,8 @@ class IeeexploreSpider(scrapy.Spider):
             end_quo = entry.find('"', begin_quo+1)
             article_name = entry[begin_quo+1: end_quo]
         return article_name
-
-    def get_doi_from_ref(self, references):
+    
+    def get_doi(self, references):
         doi = []
         for entry in references:
             article_name = self.get_article_name_from_ref(entry)
@@ -96,47 +80,83 @@ class IeeexploreSpider(scrapy.Spider):
                 except Exception as e:
                     print(e)
         return doi
-    def get_ieee_paper(self, doi):
+    
+    def get_title(self, entry):
+        if type(entry) is dict:
+            article_data = entry['articles']
+            article_name = article_data[0]["title"]      #originally named title
+        else: 
+            begin_quo = entry.find('"') 
+            article_name = ''
+            if begin_quo == -1:
+                article_name = 'None'
+            else:
+                end_quo = entry.find('"', begin_quo+1)
+                article_name = entry[begin_quo+1: end_quo]
+        return article_name
+    
+    
+    def get_authors(self, data):                       #data is a dict
+        article_data = data['articles']
+        
+        authors_data = article_data[0]['authors']['authors']
+        #authors_full_name = ''
+        #for i in authors_data:
+            #authors_full_name += i['full_name'] +', '
+
+        authors_full_names = [i['full_name'] for i in authors_data]
+        authors_full_names = ', '.join(authors_full_names)
+        return authors_full_names
+    
+    def get_abstract(self, data):
+        article_data = data['articles']
+        abstract = article_data[0]['abstract']
+        return abstract
+    
+    def get_references(self, doi):   
+        url = f'https://ieeexplore.ieee.org/xpl/dwnldReferences?arnumber={doi}'
+        html = urlopen(url).read()
+        soup = BeautifulSoup(html, features="html.parser")
+        # kill all script and style elements
+        for script in soup(["script", "style"]):
+            script.extract()    # rip it out
+        # get text
+        text = soup.get_text()   
+        #pprint(concat(c for c in text if c not in ['\n', '\t']))
+        entries= text.split('\t    \t\t\t\n')                        #splitting the articles and putting them into a list
+        entries= entries[1:]                                         # omit "View References"
+        references = [self.clean_reference(entry) for entry in entries]         #cleaning each reference and put them into a list
+        return references
+    
+    def get_paper(self, doi):
         try: 
             data = self.get_ieee_paper_dict(doi)
-
-            article_data = data['articles']
-            title = article_data[0]["title"]
-
-            authors_data = article_data[0]['authors']['authors']
-            #authors_full_name = ''
-            #for i in authors_data:
-                #authors_full_name += i['full_name'] +', '
-
-            authors_full_names = [i['full_name'] for i in authors_data]
-            authors_full_names = ', '.join(authors_full_names)
-
-            abstract = article_data[0]['abstract']
-
-            article_number = article_data[0]['article_number']
-            references = self.ieee_references(article_number)
-            #references = ', '.join(references)
+            title = self.get_title(data)
+            authors = self.get_authors(data)
+            abstract = self.get_abstract(data)
+            references = self.get_references(doi)
 
             final_data = [{
-                'DOI': doi,
-                'Title': title,
-                'Author(s)': authors_full_names,
-                'Abstract': abstract,
-                'Reference(s)': references,
-                'Referenced by': 'None'
-            }]
+                    'DOI': doi,
+                    'Title': title,
+                    'Author(s)': authors,
+                    'Abstract': abstract,
+                    'Reference(s)': references,
+                    'Referenced by': 'None'
+                }]
             return final_data
+    
         except Exception as e:
             print(e)
-
+    
     def get_all_papers(self, doi):
-        data = self.get_ieee_paper(doi)
+        data = self.get_paper(doi)
         #print(data)
-        references = self.ieee_references(doi)
-        references_doi = self.get_doi_from_ref(references)
-        print(references_doi)
+        references = self.get_references(doi)
+        references_doi = self.get_doi(references)
+        #print(references_doi)
         for item in references_doi:
-            ref_paper_data = self.get_ieee_paper(item)
+            ref_paper_data = self.get_paper(item)
             ref_paper_data[0]['Referenced by'] = data[0]['DOI']
             try:
                 data.extend(ref_paper_data)
