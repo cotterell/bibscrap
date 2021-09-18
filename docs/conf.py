cb863fb22d3359c3544e8f2fe4658e2d62745973
Michael E. Cotterell <mepcotterell@gmail.com>
2021-09-18 00:30:32 -0400

    updated docs

--- docs/conf.py
+++ docs/conf.py
@@ -10,7 +10,7 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 
-from datetime.datetime import now
+from datetime import datetime
 
 import os
 import sys
@@ -20,7 +20,9 @@ sys.path.insert(0, os.path.abspath(".."))
 # -- Project information -----------------------------------------------------
 
 project = "bibscrap"
-copyright = f"2020–{now().year}, Michael E. Cotterell and the University of Georgia"
+copyright = (
+    f"2020–{datetime.now().year}, Michael E. Cotterell and the University of Georgia"
+)
 author = "Michael E. Cotterell"
 
 
@@ -38,7 +40,6 @@ templates_path = ["_templates"]
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
-
 
 # -- Options for HTML output -------------------------------------------------
 
