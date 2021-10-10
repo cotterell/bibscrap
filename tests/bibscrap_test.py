9aabe66702b97d1096dfdc3d3f2e1c52fb2a40e5
Michael E. Cotterell <mepcotterell@gmail.com>
2021-10-09 23:24:14 -0400

    more tests

--- tests/bibscrap_test.py
+++ tests/bibscrap_test.py
@@ -1,3 +1,6 @@
+from unittest.mock import patch
+
+import bibscrap
 import unittest
 import pytest
 
@@ -5,3 +8,11 @@ import pytest
 class BibscrapTest(unittest.TestCase):
     def test_bibscrap(self):
         self.assertTrue(True)
+
+
+class BibscrapAppTest(unittest.TestCase):
+    @patch("bibscrap.builtin_extensions", ["math"])
+    def test_load_builtin_extensions(self):
+        with self.assertRaises(bibscrap.BibscrapException):
+            app = bibscrap.BibscrapApp()
+            app.load_builtin_extensions()
