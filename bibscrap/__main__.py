8bc67cf665f8824b8a983c5d771e54a2da55d051
Michael E. Cotterell <mepcotterell@gmail.com>
2021-10-14 16:20:16 -0400

    saving before transition to cleo

---
+++ bibscrap/__main__.py
@@ -0,0 +1,5 @@
+import bibscrap
+import sys
+
+if __name__ == "__main__":
+    sys.exit(bibscrap.main())
---
+++ tests/bibscrap_app_test.py
@@ -0,0 +1,4 @@
+"""Tests for the bibscrap.app module."""
+
+from bibscrap.app import BibscrapApp, builtin_extensions
+from bibscrap.errors import BibscrapError, BibscrapExtensionTypeError
